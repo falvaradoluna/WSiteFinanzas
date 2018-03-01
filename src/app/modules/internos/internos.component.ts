d72b1127ee86b0d0ea75fb627d22fc3403503bcc
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-03-01 15:57:03 -0600

    BugAcumuladoReal

--- src/app/modules/internos/internos.component.ts
+++ src/app/modules/internos/internos.component.ts
@@ -1018,6 +1018,7 @@ export class InternosComponent implements OnInit {
   }
 
   onClickUnidadesAcumuladoReal(i: number, value: number, name: string, idDetalleUnidades: number) {
+    console.log( "Aqui" );
     const concepto = this.resultadoUnidades[i].descripcion;
     const idOrigen = this.resultadoUnidades[i].idOrigen;
 
@@ -1031,6 +1032,7 @@ export class InternosComponent implements OnInit {
       // QUITAR UNA
       this.detalleUnidadesConcepto = concepto; // <-----QUITAR despues de refactorizar
       this.unidadesConcepto = concepto;
+      this.getDetalleUnidadesAcumuladoReal();
     }
   }
 
@@ -1078,6 +1080,7 @@ export class InternosComponent implements OnInit {
   }
 
   getDetalleUnidadesAcumuladoReal(): void {
+    console.log( "Hola" );
     this._service.get_AutoLineaAcumulado({
       IdCompania: this.selectedIdSucursal > 0 ?  0 : this.selectedCompania,
       IdSucursal: this.selectedIdSucursal > 0 ? this.selectedIdSucursal : 0,
