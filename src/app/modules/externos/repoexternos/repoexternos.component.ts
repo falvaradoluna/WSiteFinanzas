78dd503d700bd1b016c3f3571debc44229f0092c
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-04-18 12:15:52 -0500

    AddComponents

---
+++ src/app/modules/externos/repoexternos/repoexternos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wsf-repoexternos',
+  templateUrl: './repoexternos.component.html',
+  styleUrls: ['./repoexternos.component.scss']
+})
+export class RepoexternosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
