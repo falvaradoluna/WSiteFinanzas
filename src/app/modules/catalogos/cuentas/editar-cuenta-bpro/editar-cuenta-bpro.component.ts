e9423c81e0c9d6666335ef2e1b125fa967f89c27
msalgado008 <37191572+msalgado008@users.noreply.github.com>
2018-05-15 18:03:54 -0500

    Se elimina codigo innecesario en la modificacion de cuenta BPRO
    Se elimina codigo innecesario en la modificacion de cuenta BPRO

--- src/app/modules/catalogos/cuentas/editar-cuenta-bpro/editar-cuenta-bpro.component.ts
+++ src/app/modules/catalogos/cuentas/editar-cuenta-bpro/editar-cuenta-bpro.component.ts
@@ -38,26 +38,8 @@ export class EditarCuentaBproComponent implements OnInit {
   cuentaContableSituacion: ICuentaContableSituacion[] = [];
   departamentos: IDepartamento[] = [];
   afectaCuentaContable: IAfectaCuentaContable[] = [];
-  public validaNumeroCuenta: boolean = false;
-  public validaConcepto: boolean = false;
-  public validaGrupoSATCuentaContable: boolean = false;
-  valorEFInterno: string = '';
-  public validaNaturaleza: boolean = false;
-  public validaCuentaDetalle: boolean = false;
-  public validaNotaDictamen: boolean = false;
-  public validaDictamen: boolean = false;
   public validaGrupo1: boolean = false;
   public validaGrupo2: boolean = false;
-  public validaGrupo3: boolean = false;
-  public validaEFInterno: boolean = false;
-  public validaNotaFinanciera: boolean = false;
-  public validaDepartamento: boolean = false;
-  public validaCompaniaCuenta: boolean = false;
-  public validaDescripcionCompania: boolean = false;
-  public validaCompaniaCuentaEspecifico: boolean = false;
-  public valorDescrCompania: string;
-  public validaAfectaCuentaContable: boolean = false;
-  public validaSituacionCuentaContable: boolean = false;
   public cuentaEncontrada: boolean = false;
 
 
@@ -113,40 +95,16 @@ export class EditarCuentaBproComponent implements OnInit {
       enviarcuenta: false,
       id: 0
     };
-    /*this.cuentaContable.numeroCuenta = '';
-    this.cuentaContable.Concepto = '';
-    this.cuentaContable.idNaturaleza = 0;
-    this.cuentaContable.idCuentaDetalle = 0;
-    this.cuentaContable.idBalanceNivel01 = 0;
-    this.cuentaContable.idBalanceNivel02 = 0;
-    this.cuentaContable.idBalanceNivel03 = 0;
-    this.cuentaContable.idEstadoResultadosI = 0;
-    this.cuentaContable.idDictamen = 0;
-    this.cuentaContable.idNotasDictamen = 0;
-    this.cuentaContable.idNotasFinanciero = 0;
-    this.cuentaContable.idSituacion = 0;
-    this.cuentaContable.idSucursal = 0;
-    this.cuentaContable.idDepartamento = 0;
-    this.cuentaContable.departamento = '';
-    this.cuentaContable.idSucursalSecuencia = 0;
-    this.cuentaContable.idFlujoEfectivo = 0;
-    this.cuentaContable.idNotasFinancieras = 0;
-    this.cuentaContable.idEstadoResultadosInternoConcepto = 0;
-    this.cuentaContable.eFInterno = '';
-    this.cuentaContable.idEFInterno = 0;
-    this.cuentaContable.descripcion = '';
-    this.cuentaContable.idAfectaCuentaContable = 0;
-    this.cuentaContable.grupoSATCuentaContable = '';*/
   }
 
   // ==========================================
   //  Evalua onblur de unputText
   // ==========================================
   buscarCuentaContable(): void {
-    this.validaNumeroCuenta = (this.cuentaContable.numeroCuenta == undefined || this.cuentaContable.numeroCuenta == '');
-    this.validaNumeroCuenta = !this.validaNumeroCuenta ? this.validaEstructuraCuenta(this.cuentaContable.numeroCuenta) : true;
+    //this.validaNumeroCuenta = (this.cuentaContable.numeroCuenta == undefined || this.cuentaContable.numeroCuenta == '');
+    //this.validaNumeroCuenta = !this.validaNumeroCuenta ? this.validaEstructuraCuenta(this.cuentaContable.numeroCuenta) : true;
 
-    if (!this.validaNumeroCuenta) {
+    if (this.cuentaContable.numeroCuenta !== undefined && this.cuentaContable.numeroCuenta !== '') {
       this.getCuentaContableLocal(this.cuentaContable.numeroCuenta);
     }
   }
@@ -323,20 +281,6 @@ export class EditarCuentaBproComponent implements OnInit {
   }
 
   // ==========================================
-  //  Evalua onblur de un inputText
-  // ==========================================
-  onBlurMetodo(newValue: string, origen: string) {
-    switch (origen) {
-      //case 'grupoSat':
-      //this.validaGrupoSATCuentaContable = (this.cuentaContable.grupoSATCuentaContable == undefined || this.cuentaContable.grupoSATCuentaContable == '');
-      //break;
-      case 'descripcion':
-        this.validaConcepto = (this.cuentaContable.Concepto == undefined || this.cuentaContable.Concepto == '');
-        break;
-    }
-  }
-
-  // ==========================================
   //  Valida el numero de cuenta
   // ==========================================
   private validaEstructuraCuenta(valor) {
@@ -362,39 +306,37 @@ export class EditarCuentaBproComponent implements OnInit {
     xmlCuenta.push('<id>' + this.cuentaContable.id + '</id>');
     xmlCuenta.push('<numeroCuenta>' + this.cuentaContable.numeroCuenta + '</numeroCuenta>');
     xmlCuenta.push('<Concepto>' + this.cuentaContable.Concepto + '</Concepto>');
-    if (typeof this.cuentaContable.idNaturaleza !== 'undefined' && this.cuentaContable.idNaturaleza !== 0) {
-      xmlCuenta.push('<idNaturaleza>' + this.cuentaContable.idNaturaleza + '</idNaturaleza>');
-    } if (typeof this.cuentaContable.idCuentaDetalle !== 'undefined' && this.cuentaContable.idCuentaDetalle !== 0) {
+    if (typeof this.cuentaContable.idCuentaDetalle !== 'undefined' && this.cuentaContable.idCuentaDetalle > 0) {
       xmlCuenta.push('<idCuentaDetalle>' + this.cuentaContable.idCuentaDetalle + '</idCuentaDetalle>');
-    } if (typeof this.cuentaContable.idBalanceNivel01 !== 'undefined' && this.cuentaContable.idBalanceNivel01 !== 0) {
+    } if (typeof this.cuentaContable.idBalanceNivel01 !== 'undefined' && this.cuentaContable.idBalanceNivel01 > 0) {
       xmlCuenta.push('<idBalanceNivel01>' + this.cuentaContable.idBalanceNivel01 + '</idBalanceNivel01>');
-    } if (typeof this.cuentaContable.idBalanceNivel02 !== 'undefined' && this.cuentaContable.idBalanceNivel02 !== 0) {
+    } if (typeof this.cuentaContable.idBalanceNivel02 !== 'undefined' && this.cuentaContable.idBalanceNivel02 > 0) {
       xmlCuenta.push('<idBalanceNivel02>' + this.cuentaContable.idBalanceNivel02 + '</idBalanceNivel02>');
-    } if (typeof this.cuentaContable.idBalanceNivel03 !== 'undefined' && this.cuentaContable.idBalanceNivel03 !== 0) {
+    } if (typeof this.cuentaContable.idBalanceNivel03 !== 'undefined' && this.cuentaContable.idBalanceNivel03 > 0) {
       xmlCuenta.push('<idBalanceNivel03>' + this.cuentaContable.idBalanceNivel03 + '</idBalanceNivel03>');
-    } if (typeof this.cuentaContable.idEstadoResultadosI !== 'undefined' && this.cuentaContable.idEstadoResultadosI !== 0) {
+    } if (typeof this.cuentaContable.idEstadoResultadosI !== 'undefined' && this.cuentaContable.idEstadoResultadosI > 0) {
       xmlCuenta.push('<idEstadoResultadosI>' + this.cuentaContable.idEstadoResultadosI + '</idEstadoResultadosI>');
-    } if (typeof this.cuentaContable.idDictamen !== 'undefined' && this.cuentaContable.idDictamen !== 0) {
+    } if (typeof this.cuentaContable.idDictamen !== 'undefined' && this.cuentaContable.idDictamen > 0) {
       xmlCuenta.push('<idDictamen>' + this.cuentaContable.idDictamen + '</idDictamen>');
-    } if (typeof this.cuentaContable.idNotasDictamen !== 'undefined' && this.cuentaContable.idNotasDictamen !== 0) {
+    } if (typeof this.cuentaContable.idNotasDictamen !== 'undefined' && this.cuentaContable.idNotasDictamen > 0) {
       xmlCuenta.push('<idNotasDictamen>' + this.cuentaContable.idNotasDictamen + '</idNotasDictamen>');
-    } if (typeof this.cuentaContable.idNotasFinanciero !== 'undefined' && this.cuentaContable.idNotasFinanciero !== 0) {
+    } if (typeof this.cuentaContable.idNotasFinanciero !== 'undefined' && this.cuentaContable.idNotasFinanciero > 0) {
       xmlCuenta.push('<idNotasFinanciero>' + this.cuentaContable.idNotasFinanciero, null + '</idNotasFinanciero>');
-    } if (typeof this.cuentaContable.idSituacion !== 'undefined' && this.cuentaContable.idSituacion !== 0) {
+    } if (typeof this.cuentaContable.idSituacion !== 'undefined' && this.cuentaContable.idSituacion > 0) {
       xmlCuenta.push('<idSituacion>' + this.cuentaContable.idSituacion + '</idSituacion>');
-    } if (typeof this.cuentaContable.idDepartamento !== 'undefined' && this.cuentaContable.idDepartamento !== 0) {
+    } if (typeof this.cuentaContable.idDepartamento !== 'undefined' && this.cuentaContable.idDepartamento > 0) {
       xmlCuenta.push('<idDepartamento>' + this.cuentaContable.idDepartamento + '</idDepartamento>');
-    } if (typeof this.cuentaContable.idSucursalSecuencia !== 'undefined' && this.cuentaContable.idSucursalSecuencia !== 0) {
+    } if (typeof this.cuentaContable.idSucursalSecuencia !== 'undefined' && this.cuentaContable.idSucursalSecuencia > 0) {
       xmlCuenta.push('<idSucursalSecuencia>' + this.cuentaContable.idSucursalSecuencia + '</idSucursalSecuencia>');
-    } if (typeof this.cuentaContable.idFlujoEfectivo !== 'undefined' && this.cuentaContable.idFlujoEfectivo !== 0) {
+    } if (typeof this.cuentaContable.idFlujoEfectivo !== 'undefined' && this.cuentaContable.idFlujoEfectivo > 0) {
       xmlCuenta.push('<idFlujoEfectivo>' + this.cuentaContable.idFlujoEfectivo + '</idFlujoEfectivo>');
-    } if (typeof this.cuentaContable.idEstadoResultadosInternoConcepto !== 'undefined' && this.cuentaContable.idEstadoResultadosInternoConcepto !== 0) {
+    } if (typeof this.cuentaContable.idEstadoResultadosInternoConcepto !== 'undefined' && this.cuentaContable.idEstadoResultadosInternoConcepto > 0) {
       xmlCuenta.push('<idEstadoResultadosInternoConcepto>' + this.cuentaContable.idEstadoResultadosInternoConcepto + '</idEstadoResultadosInternoConcepto>');
-    } if (typeof this.cuentaContable.idEFInterno !== 'undefined' && this.cuentaContable.idEFInterno !== 0) {
+    } if (typeof this.cuentaContable.idEFInterno !== 'undefined' && this.cuentaContable.idEFInterno > 0) {
       xmlCuenta.push('<idEFInterno>' + this.cuentaContable.idEFInterno + '</idEFInterno>');
-    } if (typeof this.cuentaContable.idAfectaCuentaContable !== 'undefined' && this.cuentaContable.idAfectaCuentaContable !== 0) {
+    } if (typeof this.cuentaContable.idAfectaCuentaContable !== 'undefined' && this.cuentaContable.idAfectaCuentaContable > 0) {
       xmlCuenta.push('<idAfectaCuentaContable>' + this.cuentaContable.idAfectaCuentaContable + '</idAfectaCuentaContable>');
-    } if (typeof this.cuentaContable.grupoSATCuentaContable !== 'undefined' && this.cuentaContable.grupoSATCuentaContable !== '') {
+    } if (typeof this.cuentaContable.grupoSATCuentaContable !== 'undefined' && this.cuentaContable.grupoSATCuentaContable > '') {
       xmlCuenta.push('<grupoSATCuentaContable>' + this.cuentaContable.grupoSATCuentaContable + '</grupoSATCuentaContable>');
     }
     //xmlCuenta.push('<eFInterno>' + this.valorEFInterno + '</eFInterno>');
@@ -416,52 +358,16 @@ export class EditarCuentaBproComponent implements OnInit {
   // ==========================================
   onChangeValidador(newValue: number, origen: string): void {
     switch (origen) {
-      case 'naturaleza':
-        this.validaNaturaleza = newValue == 0;
-        break;
-      case 'cuentaDetalle':
-        this.validaCuentaDetalle = newValue == 0;
-        break;
-      case 'notaDictamen':
-        this.validaNotaDictamen = newValue == 0;
-        break;
-      case 'dictamen':
-        this.validaDictamen = newValue == 0;
-        break;
       case 'grupo1':
-        this.validaGrupo1 = newValue == 0;
-        if (!this.validaGrupo1) {
+        if (newValue > 0) {
           this.getBalanceConceptoNivel02();
         }
         break;
       case 'grupo2':
-        this.validaGrupo2 = newValue == 0;
-        if (!this.validaGrupo1 && !this.validaGrupo2) {
+        if (this.cuentaContable.idBalanceNivel01 > 0 && this.cuentaContable.idBalanceNivel02 > 0) {
           this.getBalanceConceptoNivel03();
         }
         break;
-      case 'grupo3':
-        this.validaGrupo3 = newValue == 0;
-        break;
-      case 'EFInterno':
-        this.validaEFInterno = newValue == 0;
-        if (newValue > 0) {
-          this.valorEFInterno = this.estadoFinancieroInterno.find(x => x.id == newValue).EstadoFinancieroInterno;
-        }
-        break;
-      case 'notaFinanciera':
-        this.validaNotaFinanciera = newValue == 0;
-        break;
-      case 'departamento':
-        this.validaDepartamento = newValue == 0;
-        break;
-      case 'afectaCuentaContable':
-        this.validaAfectaCuentaContable = newValue == -1;
-        break;
-      case 'situacionCuenta':
-        this.validaSituacionCuentaContable = newValue == -1;
-        break;
-
     }
   }
 
@@ -479,67 +385,31 @@ export class EditarCuentaBproComponent implements OnInit {
   //  Guarda la información de la cuenta
   // ==========================================
   guardarCuenta(): void {
-    if (!this.validaFormulario()) {
-      this.controlaSpinner(true, 3000);
-      const usuario = JSON.parse(localStorage.getItem('userLogged'));
-      this._cuentaContableService.CuentaContableEditar({
-        idUsuario: usuario.id,
-        xmlCuenta: this.getXmlCuenta()
-      })
-        .subscribe(
-          resultadoInsert => {
+    this.controlaSpinner(true, 3000);
+    const usuario = JSON.parse(localStorage.getItem('userLogged'));
+    this._cuentaContableService.CuentaContableEditar({
+      idUsuario: usuario.id,
+      xmlCuenta: this.getXmlCuenta()
+    })
+      .subscribe(
+        resultadoInsert => {
+          this.controlaSpinner(false);
+          if (resultadoInsert.respuesta === 1) {
+            this.inicializaCuenta();
+            this.cuentaEncontrada = false;
+            swal('Administración de cuenta', 'Cuenta actualizada correctamente', 'success');
+          } else {
             this.controlaSpinner(false);
-            if (resultadoInsert.respuesta === 1) {
-              this.inicializaCuenta();
-              this.cuentaEncontrada = false;
-              swal('Administración de cuenta', 'Cuenta actualizada correctamente', 'success');
-            } else {
-              this.controlaSpinner(false);
-              swal('Administración de cuenta', 'Ocurrio un error al actualizar la cuenta', 'error');
-            }
-          },
-          error => this.errorMessage = <any>error
-        );
-    }
+            swal('Administración de cuenta', 'Ocurrio un error al actualizar la cuenta', 'error');
+          }
+        },
+        error => this.errorMessage = <any>error
+      );
   }
 
   ocultarFormmulario(): void {
     this.inicializaCuenta();
     this.cuentaEncontrada = false;
-  }
-
-  // ==========================================
-  //  Valida la información del formulario
-  // ==========================================
-  private validaFormulario() {
-    /*
-    this.validaNaturaleza = this.cuentaContable.idNaturaleza == 0;
-    this.validaCuentaDetalle = this.cuentaContable.idCuentaDetalle == 0;
-    this.validaNotaDictamen = this.cuentaContable.idNotasDictamen == 0;
-    this.validaDictamen = this.cuentaContable.idDictamen == 0;
-    this.validaGrupo1 = this.cuentaContable.idBalanceNivel01 == 0;
-    this.validaGrupo2 = this.cuentaContable.idBalanceNivel02 == 0;
-    this.validaGrupo3 = this.cuentaContable.idBalanceNivel03 == 0;
-    this.validaEFInterno = this.cuentaContable.idEFInterno == 0;
-    this.validaNotaFinanciera = this.cuentaContable.idNotasFinancieras == 0;
-    this.validaDepartamento = this.cuentaContable.idDepartamento == 0;
-    this.validaNumeroCuenta = (this.cuentaContable.numeroCuenta == undefined || this.cuentaContable.numeroCuenta == '');
-    this.validaNumeroCuenta = !this.validaNumeroCuenta ? this.validaEstructuraCuenta(this.cuentaContable.numeroCuenta) : true;
-    this.validaConcepto = (this.cuentaContable.Concepto == undefined || this.cuentaContable.Concepto == '');
-    this.validaAfectaCuentaContable = this.cuentaContable.idAfectaCuentaContable == -1;
-    this.validaGrupoSATCuentaContable = (this.cuentaContable.grupoSATCuentaContable == undefined || this.cuentaContable.grupoSATCuentaContable == '');
-    this.validaSituacionCuentaContable = this.cuentaContable.idSituacion == 0;
-    */
-
-    this.validaNumeroCuenta = !this.validaNumeroCuenta ? this.validaEstructuraCuenta(this.cuentaContable.numeroCuenta) : true;
-    this.validaConcepto = (this.cuentaContable.Concepto == undefined || this.cuentaContable.Concepto == '');
-    //this.validaGrupoSATCuentaContable = (this.cuentaContable.grupoSATCuentaContable == undefined || this.cuentaContable.grupoSATCuentaContable == '');
-    /*return (this.validaNaturaleza || this.validaCuentaDetalle || this.validaNotaDictamen || this.validaDictamen
-      || this.validaGrupo1 || this.validaGrupo2 || this.validaGrupo3 || this.validaNotaFinanciera || this.validaEFInterno
-      || this.validaDepartamento || this.validaCompaniaCuenta || this.validaNumeroCuenta || this.validaConcepto
-      || this.validaAfectaCuentaContable || this.validaGrupoSATCuentaContable || this.validaSituacionCuentaContable)*/
-    return (this.validaNumeroCuenta || this.validaConcepto)
-
   }
 
 }
